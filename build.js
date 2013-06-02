50a03cf27597b31769c95ae67fa286b3e585edc4
Ethan Truong <etruong42@gmail.com>
2013-06-02 15:17:38 -0400

    update build and requirejs

--- build.js
+++ build.js
@@ -2,8 +2,10 @@
 	baseUrl: 'server/public/assets/js',
 	mainConfigFile: 'server/public/assets/js/main.js',
 	name: 'main',
-	out: '../etruong42.github.com/wordslinger/assets/js/scripts.min.js',
+	out: 'server/public/assets/js/scripts.min.js',
 	preserveLicenseComments: false,
+	generateSourceMaps: true,
+	optimize: 'uglify2',
 	paths: {
 		requireLib: 'vendor/require'
 	},
